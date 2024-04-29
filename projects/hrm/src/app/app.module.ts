8972335c0271ec0a5df224dd63c659ae9be3d6d4
LHTrungSkySP <lehoangtrung038582@gmail.com>
2024-04-30 02:22:49 +0700

    angular-architects/module-federation

--- projects/hrm/src/app/app.module.ts
+++ projects/hrm/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
-import { AppComponent } from './app.component';
 
 
 
